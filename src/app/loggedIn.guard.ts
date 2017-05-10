37227a76576c75f3719cb97cea13bddd12882e1d
serse83 <sergio.marzullo@gmail.com>
2017-05-10 18:07:10 +0200

    Add files via upload

--- src/app/loggedIn.guard.ts
+++ src/app/loggedIn.guard.ts
@@ -3,14 +3,23 @@ import { Injectable } from '@angular/core';
 
 //CanActivate - Decides if a route can be activated
 import { CanActivate } from '@angular/router';
+
 //to put authservice in protected router 
 import { AuthService } from './AuthService';
 
+import {LoginComponent} from './Logincomponent'
+
 @Injectable()
+
 export class LoggedInGuard implements CanActivate {
+
   constructor(private authService: AuthService) {}
+
 //active the protected routed and make the return user 
+
   canActivate(): boolean {
+
     return this.authService.Logged();
   }
+
 }
